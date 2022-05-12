6a2076fd2a3f5f64186332edf55eeaf5f550dd9c
camrynalexx <camryn.cam@gmail.com>
2022-05-12 03:27:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">
+        Hi
+      </button>
+    </div>
+  );
+}
+
+export default App;
